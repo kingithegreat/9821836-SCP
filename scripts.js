947188e307614ad507fddf3e2e59d8a563fd3504
kingithegreat <139932842+kingithegreat@users.noreply.github.com>
2024-04-04 13:46:43 +1300

    added footer and styles

--- scripts.js
+++ scripts.js
@@ -6,7 +6,3 @@
 function showAlert() {
     alert("YOU ARE NOT AUTHORIZED TO VIEW THIS CONTENT"); }
 
-    // Initialization for ES Users
-import { Ripple, initMDB } from "mdb-ui-kit";
-
-initMDB({ Ripple });
